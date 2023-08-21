470e4fbcfd2dcd8f00f19bb8b7b4766cc512fa0c
Miguel A <aliagaf404@gmail.com>
2023-08-21 17:10:25 -0500

    navbar: ok todo: home content

--- src/components/NavBar/NavLoginModal.tsx
+++ src/components/NavBar/NavLoginModal.tsx
@@ -1,85 +1,110 @@
-import { Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Checkbox, Input, Button, Code } from "@nextui-org/react";
+import {
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Checkbox,
+  Input,
+  Button,
+  Code,
+  Avatar,
+} from "@nextui-org/react";
 import { signIn, useSession } from "next-auth/react";
 import { AppRouterInstance } from "next/dist/shared/lib/app-router-context";
 import Link from "next/link";
 import React, { FormEvent, useState } from "react";
 import { IoMdLock, IoMdMail, IoMdPerson } from "react-icons/io";
 
-function NavLoginModal(props: { isOpen: boolean; onClose: () => void, router: any }) {
+function NavLoginModal(props: {
+  isOpen: boolean;
+  onClose: () => void;
+  router: any;
+}) {
+  const [error, setError] = useState("");
 
-  const [error, setError] = useState("")
+  const handleLoginBtn = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
 
-
-  const handleLoginBtn = async (e: FormEvent<HTMLFormElement>)=>{
-      e.preventDefault();
-
-      const formData = new FormData(e.currentTarget);
-      const res = await signIn('credentials', {
-        email: formData.get('email'),
-        password: formData.get('password'),
+    const formData = new FormData(e.currentTarget);
+    const res = await signIn("credentials", {
+      email: formData.get("email"),
+      password: formData.get("password"),
       redirect: false,
     });
 
-    if (res?.error) return setError(res.error as string)
+    if (res?.error) return setError(res.error as string);
 
-    if(res?.ok){
-      if(props.isOpen) props.onClose()
+    if (res?.ok) {
+      if (props.isOpen) props.onClose();
 
-      props.router.push("/dashboard/")
-    } 
-  }
+      props.router.push("/dashboard/");
+    }
+  };
   return (
     <>
-      <Modal 
-        isOpen={props.isOpen} 
+      <Modal
+        isOpen={props.isOpen}
+        className="overflow-y-auto h-auto"
         onOpenChange={props.onClose}
         placement="top-center"
       >
         <form onSubmit={handleLoginBtn}>
-        <ModalContent>
-          {(onClose) => (
-            <>
-            
-              <ModalHeader className="flex flex-col gap-1 text-center">Iniciar sesión</ModalHeader>
-              <ModalBody>
-              {error?<Code color="danger">{error}</Code>:""}
-                <Input
-                  autoFocus
-                  endContent={
-                    <IoMdMail className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />
-                  }
-                  name="email"
-                  label="Correo electrónico"
-                  required
-                  placeholder="Ingrese su correo electrónico"
-                  variant="underlined"
-                />
-                <Input
-                  endContent={
-                    <IoMdLock className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />
-                  }
-                  label="Contraseña"
-                  name="password"
-                  required
-                  placeholder="Ingrese su cotnraseña"
-                  type="password"
-                  variant="underlined"
-                />
-              </ModalBody>
-              <ModalFooter>
-                <Button color="danger" variant="ghost" onPress={onClose}>
-                  Close
-                </Button>
-                <Button color="primary" variant="ghost" type="submit">
-                  Sign in
-                </Button>
-              </ModalFooter>
-            </>
-          )}
-        </ModalContent>
+          <ModalContent>
+            {(onClose) => (
+              <>
+                <ModalHeader className="w-full justify-center items-center">
+                  Iniciar sesión
+                </ModalHeader>
+                <ModalBody>
+                  <div className="grid md:grid-cols-2">
+                    <div className="md:w-2/6 w-full h-auto flex justify-center items-center">
+                      <Avatar
+                        src={"background.jpg"}
+                        className="w-32 h-32 text-large"
+                      />
+                    </div>
+
+                    <div className="w-full md:w-4/6 px-4 flex flex-col items-center justify-center">
+                      {error ? <Code color="danger">{error}</Code> : ""}
+                      <Input
+                        autoFocus
+                        endContent={
+                          <IoMdMail className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />
+                        }
+                        name="email"
+                        label="Correo electrónico"
+                        required
+                        placeholder="Ingrese su correo electrónico"
+                        variant="underlined"
+                      />
+                      <Input
+                        endContent={
+                          <IoMdLock className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />
+                        }
+                        label="Contraseña"
+                        name="password"
+                        required
+                        placeholder="Ingrese su cotnraseña"
+                        type="password"
+                        variant="underlined"
+                      />
+                    </div>
+                  </div>
+                </ModalBody>
+                <ModalFooter>
+                  <Button color="danger" variant="ghost" onPress={onClose}>
+                    Close
+                  </Button>
+                  <Button color="primary" variant="ghost" type="submit">
+                    Sign in
+                  </Button>
+                </ModalFooter>
+              </>
+            )}
+          </ModalContent>
         </form>
       </Modal>
-      
     </>
   );
 }
--- src/components/NavBar/NavRegisterModal.tsx
+++ src/components/NavBar/NavRegisterModal.tsx
@@ -16,9 +16,7 @@ import {
 } from "@nextui-org/react";
 import axios, { AxiosError } from "axios";
 import { signIn } from "next-auth/react";
-import { AppRouterInstance } from "next/dist/shared/lib/app-router-context";
-import Link from "next/link";
-import { useRouter } from "next/navigation";
+import {Select, SelectItem} from "@nextui-org/react";
 import React, { FormEvent, useState } from "react";
 import {
   IoMdCode,
@@ -26,205 +24,235 @@ import {
   IoMdMail,
   IoMdPerson,
   IoMdPersonAdd,
-  IoMdReorder,
   IoMdSchool,
   IoMdTime,
 } from "react-icons/io";
 
-function NavRegisterModal(props: { isOpen: boolean; onClose: () => void, router:any }) {
+function NavRegisterModal(props: {
+  isOpen: boolean;
+  onClose: () => void;
+  router: any;
+}) {
   const [selected, setSelected] = useState("Seleccione un género");
   const [error, setError] = useState("");
 
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    let age = 0 
-      
+    let age = 0;
 
-
-      try {
-        const formData = new FormData(e.currentTarget)
-        const password = formData.get("password")
-        const repassword = formData.get('repassword')
-        const fechaActual = Date.now()
+    try {
+      const formData = new FormData(e.currentTarget);
+      const password = formData.get("password");
+      const repassword = formData.get("repassword");
+      const fechaActual = Date.now();
       const edadValue = formData.get("edad")?.toString();
       if (edadValue) {
-        const edad = fechaActual- new Date(edadValue).getDate();
-        age = edad
+        const edad = fechaActual - new Date(edadValue).getDate();
+        age = edad;
       } else {
-        console.log("edad error...")
+        console.log("edad error...");
       }
-        if( password === repassword ){
-          const axiosResponse = await axios.post("/api/auth/signup", {
-            nombreCompleto: formData.get("nombreCompleto"),
-            genero: formData.get("genero"),
-            grado: formData.get("grado"),
-            password: password,
-            email: formData.get("email"),
-            edad: age
+      if (password === repassword) {
+        const axiosResponse = await axios.post("/api/auth/signup", {
+          nombreCompleto: formData.get("nombreCompleto"),
+          genero: formData.get("genero"),
+          grado: formData.get("grado"),
+          password: password,
+          email: formData.get("email"),
+          edad: age,
+        });
+        setError((await axiosResponse).statusText);
+        if (error === "") {
+          const res = await signIn("credentials", {
+            email: axiosResponse.data.email,
+            password: formData.get("password"),
+            redirect: false,
           });
-          setError((await axiosResponse).statusText)
-          if(error === "") {
-            const res = await signIn('credentials', {
-              email: axiosResponse.data.email,
-              password: formData.get("password"),
-              redirect: false
-            })
-  
-            console.log(res)
-  
-            if(res?.ok){
-              if(props.isOpen) props.onClose()
 
-              props.router.push("/dashboard/")
-          
-            } 
+          console.log(res);
+
+          if (res?.ok) {
+            if (props.isOpen) props.onClose();
+
+            props.router.push("/dashboard/");
           }
-        }else{
-          const errorMessage = "Las contraseñas no coinciden."
-          setError(errorMessage)
         }
-      } catch (error) {
-        console.log(error);
-        if (error instanceof AxiosError) {
-          const errorMessage = error.response?.data.message;
-          setError(errorMessage);
-        } else {
-          setError(String(error));
-        }
+      } else {
+        const errorMessage = "Las contraseñas no coinciden.";
+        setError(errorMessage);
       }
-
-   
+    } catch (error) {
+      console.log(error);
+      if (error instanceof AxiosError) {
+        const errorMessage = error.response?.data.message;
+        setError(errorMessage);
+      } else {
+        setError(String(error));
+      }
+    }
   };
   return (
     <>
-      <Modal backdrop={"blur"} isOpen={props.isOpen} className="overflow-y-auto md:h-max h-4/5" onClose={props.onClose}>
+      <Modal
+        backdrop={"blur"}
+        isOpen={props.isOpen}
+        className="overflow-y-auto md:h-max h-4/5"
+        onClose={props.onClose}
+      >
         <form onSubmit={handleSubmit}>
-        <ModalContent className="text-center ">
-          {(onClose) => (
-            <>
-              <ModalHeader className="flex flex-col w-full h-max justify-center items-center gap-1">
-                <div className="flex w-full h-max justify-center items-center gap-1" >
-                <IoMdPersonAdd />
-                <p>Regístrate</p>
-                </div>
+          <ModalContent className="text-center ">
+            {(onClose) => (
+              <>
+                <ModalHeader className="flex flex-col w-full h-max text-primary justify-center items-center gap-1">
+                  <div className="flex w-full h-max justify-center items-center gap-1">
+                    <IoMdPersonAdd />
+                    <p className="pl-2">Regístrate</p>
+                  </div>
+                </ModalHeader>
+                <ModalBody className="">
+                  {error ? (
+                    <Code
+                      color="danger"
+                      className="w-auto flex-col h7 whitespace-pre-wrap"
+                    >
+                      <p className="text-clip">{error}</p>
+                    </Code>
+                  ) : (
+                    ""
+                  )}
 
-                {error?<Code color="danger">{error}</Code>:""}
-              </ModalHeader>
-              <ModalBody className="">
-                <Card>
-                  <CardBody>
-                    <div className="md:grid md:grid-cols-2 flex flex-col grap-2">
-                      <div className="px-2 py-1">
-                        <Input
-                          required
-                          type="text"
-                          label="Nombre completo"
-                          placeholder="Ej: Miguel Aliaga Chacón"
-                          startContent={<IoMdPerson />}
-                          name="nombreCompleto"
-                        />
-                      </div>
-                      <div className="px-2 py-1">
-                      <Input
-                          type="email"
-                          label="Correo electrónico"
-                          placeholder="Ej. uncorreo@email.com"
-                          startContent={< IoMdMail/>}        
-                          name="email"                
-                        />
-                      </div>
-                      <div className="px-2 py-1">
-                        <Card className="text-foreground">
-                        <CardBody>
-                        <RadioGroup className="text-default"
-                          label="Seleccione su género"
-                          value={selected}
-                          onValueChange={setSelected}
-                          name="genero"
-                        >
-                          <Radio value="M">Masculino</Radio>
-                          <Radio value="F">Fememino</Radio>
-                          
-                        </RadioGroup>
-                        </CardBody>
-                        </Card>
-                      </div>
-                      <div className="px-2 py-1 flex-col">
-                        <Input
-                          type="number"
-                          label="Grado"
-                          placeholder="Ingrese su grado"
-                          startContent={<IoMdSchool />}
-                          className="py-2"
-                          name="grado"
-                          max={5}
-                          min={1}
-                        />
-                        <Input
-                          type="date"
-                          label="Fecha de nacimiento"
-                          name="edad"
-                          startContent={<IoMdTime />}
-                          className="py-2"
-                        />
-                      </div>
-                      <div className="px-2 py-1">
-                      <Input
-                          type="password"
-                          label="Contraseña"
-                          name="password"
-                          placeholder="Cree una contraseña"
-                          startContent={< IoMdLock/>}
-                          className="py-2"
-                        />
-                      </div>
-                      <div className="px-2 py-1">
-                      <Input
-                          type="password"
-                          label="Re-contraseña"
-                          name="repassword"
-                          placeholder="Confirme su contraseña"
-                          startContent={< IoMdLock/>}
-                          className="py-2"
-                        />
-                      </div>
+                  <Card>
+                    <CardBody>
+                      <div className="md:grid md:grid-cols-2 flex flex-col grap-2">
+                        <div className="px-2 py-1">
+                          <Input
+                            required
+                            type="text"
+                            label="Nombre completo"
+                            color="primary"
+                            placeholder="Ej: Miguel Aliaga Chacón"
+                            startContent={<IoMdPerson />}
+                            variant="underlined"
+                            name="nombreCompleto"
+                          />
+                        </div>
+                        <div className="px-2 py-1">
+                          <Input
+                            type="email"
+                            label="Correo electrónico"
+                            color="primary"
+                            placeholder="Ej. uncorreo@email.com"
+                            startContent={<IoMdMail />}
+                            variant="underlined"
+                            name="email"
+                          />
+                        </div>
+                        <div className="px-2 py-1">
+                          <Select
+                            label="Género"
+                            placeholder="Seleccione su género"
+                            className="py-2"
+                            color="primary"
+                            variant="underlined"
+                            name="genero"
+                          >
+                              <SelectItem key={"M"}>
+                                Masculino
+                              </SelectItem>
+                              <SelectItem key={"F"}>
+                                Femenino
+                              </SelectItem>
                       
-                     <div className="col-span-2 px-2 flex flex-col justify-center items-center">
-                     <Input
-                          type="password"
-                          name="codigoInvitacion"
-                          label="Código de invitación"
-                          placeholder="Ingrese su código"
-                          startContent={< IoMdCode/>}
-                          className="py-2"
-                        />
-                     </div>
-                    </div>
-                  </CardBody>
-                </Card>
-              </ModalBody>
-              <ModalFooter className="w-full justify-center">
-                <Button
-                  color="success"
-                  href="/dashboard/profile"
-                  variant="ghost"
-                  type="submit"
-                  
-                >
-                  Crear cuenta
-                </Button>
-                <Button
-                  color={"danger"}
-                  variant="ghost"
-                  onClick={props.onClose}
-                >
-                  Regresar
-                </Button>
-              </ModalFooter>
-            </>
-          )}
-        </ModalContent>
+                          </Select>
+                        </div>
+                        <div className="px-2 py-1">
+                          <Input
+                            type="number"
+                            label="Grado"
+                            color="primary"
+                            placeholder="Ingrese su grado"
+                            startContent={<IoMdSchool />}
+                            className="py-2"
+                            name="grado"
+                            variant="underlined"
+                            max={5}
+                            min={1}
+                          />
+                          
+                        </div>
+                        <div className="px-2 py-1">
+                        <Input
+                            type="date"
+                            label="Fecha de nacimiento"
+                            name="edad"
+                            color="primary"
+                            variant="underlined"
+                            startContent={<IoMdTime />}
+                            className="py-2"
+                          />                          
+                        </div>
+                        <div className="px-2 py-1">
+                          <Input
+                            type="password"
+                            label="Contraseña"
+                            color="primary"
+                            variant="underlined"
+                            name="password"
+                            placeholder="Cree una contraseña"
+                            startContent={<IoMdLock />}
+                            className="py-2"
+                          />
+                        </div>
+                        <div className="px-2 py-1">
+                          <Input
+                            type="password"
+                            label="Re-contraseña"
+                            name="repassword"
+                            color="primary"
+                            variant="underlined"
+                            placeholder="Confirme su contraseña"
+                            startContent={<IoMdLock />}
+                            className="py-2"
+                          />
+                        </div>
+
+                        <div className="px-2 py-1">
+                          <Input
+                            type="password"
+                            name="codigoInvitacion"
+                            label="Código de invitación"
+                            color="primary"
+                            variant="underlined"
+                            placeholder="Ingrese su código"
+                            startContent={<IoMdCode />}
+                            className="py-2"
+                          />
+                        </div>
+                      </div>
+                    </CardBody>
+                  </Card>
+                </ModalBody>
+                <ModalFooter className="w-full justify-center">
+                  <Button
+                    color="success"
+                    href="/dashboard/profile"
+                    variant="ghost"
+                    type="submit"
+                  >
+                    Crear cuenta
+                  </Button>
+                  <Button
+                    color={"danger"}
+                    variant="ghost"
+                    onClick={props.onClose}
+                  >
+                    Regresar
+                  </Button>
+                </ModalFooter>
+              </>
+            )}
+          </ModalContent>
         </form>
       </Modal>
     </>
