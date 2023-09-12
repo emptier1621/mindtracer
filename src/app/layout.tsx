eb38a534cecc80af310018df58f7b8d6d40970a0
Miguel A <aliagaf404@gmail.com>
2023-09-12 17:42:00 -0500

    nzxzxczzxc

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import Head from 'next/head'
 import './globals.css'
 import Providers from './Providers'
+import { Session } from 'next-auth'
+import { getSession } from 'next-auth/react'
 
 export const metadata = {
   title: 'MindTracer',
@@ -12,11 +14,13 @@ export default function RootLayout ({
 }: {
   children: React.ReactNode
 }) {
+  const session = getSession()
+  console.log(session)
   return (
     <html lang='es' className='dark'>
       <body className='max-w-screen'>
         <div>
-          <Providers>{children}</Providers>
+          <Providers session={session}>{children}</Providers>
         </div>
       </body>
     </html>
