de23e16fd15790f963f325bc1ab89f5e3b911fb5
Miguel A <aliagaf404@gmail.com>
2023-09-12 17:03:15 -0500

    ccc

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,27 +1,22 @@
 import Head from 'next/head'
 import './globals.css'
 import Providers from './Providers'
-import { getSession } from 'next-auth/react'
-import { Session } from 'next-auth'
 
 export const metadata = {
   title: 'MindTracer',
   description: 'Detectar síntomas de depresión con inteligencia artificial.'
 }
 
-
-
 export default function RootLayout ({
-  children,session
+  children
 }: {
-  children: React.ReactNode,
-  session: { session: Session | null } & Record<string, unknown>;
+  children: React.ReactNode
 }) {
   return (
     <html lang='es' className='dark'>
       <body className='max-w-screen'>
         <div>
-          <Providers session={session}>{children}</Providers>
+          <Providers>{children}</Providers>
         </div>
       </body>
     </html>
